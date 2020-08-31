0f6f3625edc759a3138c27edb03d244dbd839e8a
naf <naf@auri.ga>
2020-08-31 14:56:56 +0900

    add Task Controller

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('tasks', 'TasksController')->only([
+    'index', 'store', 'edit', 'update', 'destroy'
+]);
 
 Auth::routes();
 
